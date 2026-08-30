ce69a70dbc4926180b9354c2a431f9e0d12f8b02
tomatenkuchen <t.w92@gmx.de>
2026-08-30 06:05:06 +0200

    hw mods

--- sw/main/main.cpp
+++ sw/main/main.cpp
@@ -24,6 +24,8 @@ extern "C" void app_main()
 
     bsp::valves::Valves valves;
 
+    bool valve1 = false;
+
     while (true)
     {
         ESP_LOGI(TAG, "main loop");
@@ -31,16 +33,24 @@ extern "C" void app_main()
 
         ESP_LOGI(TAG, "USB voltage online: %s", power.is_usb_voltage_online() ? "yes" : "no");
 
-        ESP_LOGI(TAG, "voltages: \n\tsolar: %d mV\n\tbattery: %dmV\n\n",
+        ESP_LOGI(TAG, "voltages: \n\tsolar: %d mV\n\tbattery: %dmV",
                  power.get_voltage_mV(bsp::Power::AdcInputs::v_solar),
                  power.get_voltage_mV(bsp::Power::AdcInputs::v_battery));
 
-        ESP_LOGI(TAG, "currents: \n\tsolar: %d mA\n\tconductivity: %dmA\n\tusb: %d mA\n\n",
+        ESP_LOGI(TAG, "currents: \n\tsolar: %d mA\n\tconductivity: %dmA\n\tusb: %d mA",
                  power.get_voltage_mV(bsp::Power::AdcInputs::i_solar),
                  power.get_voltage_mV(bsp::Power::AdcInputs::i_conductivity),
                  power.get_voltage_mV(bsp::Power::AdcInputs::i_usb));
 
+        // conductivity
         v_cond_out_mV += 1024;
         power.set_conductivity_voltage_mV(v_cond_out_mV);
+
+        ESP_LOGI(TAG, "conductivity voltage: %d\n", v_cond_out_mV);
+
+        // valves
+        valves.enable_valve_1(valve1);
+        ESP_LOGI(TAG, "valve1: %s", valve1 ? "on" : "off");
+        valve1 = !valve1;
     }
 }
