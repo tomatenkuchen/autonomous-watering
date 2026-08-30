e132599c204e92e4bd278390bba93789cc9077ad
tomatenkuchen <t.w92@gmx.de>
2026-08-30 07:03:44 +0200

    start buck formular

--- sw/main/main.cpp
+++ sw/main/main.cpp
@@ -13,18 +13,30 @@ namespace
 {
 
 const static char *TAG = "main";
+bsp::Power *power_ptr = nullptr;
+
+auto charge_battery() -> void
+{
+    auto const v_bat = power_ptr->get_voltage_mV(bsp::Power::AdcInputs::v_battery);
+    auto const v_solar = power_ptr->get_voltage_mV(bsp::Power::AdcInputs::v_solar);
+
+    // calculate duty for power transfer
+    float const i_bat_ref_mA = 100.0;
+    float const L_uH = 22.0;
+}
 
 } // namespace
 
 extern "C" void app_main()
 {
     bsp::Power power;
+    power_ptr = &power;
 
     std::uint16_t v_cond_out_mV = 0;
 
     bsp::valves::Valves valves;
 
-    bool valve1 = false;
+    std::uint8_t valve_cnt = 0;
 
     while (true)
     {
@@ -49,8 +61,14 @@ extern "C" void app_main()
         ESP_LOGI(TAG, "conductivity voltage: %d\n", v_cond_out_mV);
 
         // valves
-        valves.enable_valve_1(valve1);
-        ESP_LOGI(TAG, "valve1: %s", valve1 ? "on" : "off");
-        valve1 = !valve1;
+        bool const v1 = valve_cnt & 1;
+        bool const v2 = (valve_cnt >> 1) & 2;
+        valves.enable_valve_1(v1);
+        valves.enable_valve_2(v2);
+        ESP_LOGI(TAG, "valve1: %s", v1 ? "on" : "off");
+        ESP_LOGI(TAG, "valve2: %s", v2 ? "on" : "off");
+        valve_cnt++;
+
+        // power transfer to battery
     }
 }
